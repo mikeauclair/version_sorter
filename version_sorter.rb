c930221229ae3c7daa9cf14c73d7891f9cde54f8
Elijah Miller <elijah.miller@gmail.com>
2009-10-09 14:21:09 -0400

    Added array_sort_hash and array_sort_array that are about 10x faster but fail when comparing "3.0.0" to "yui3-123".

--- version_sorter.rb
+++ version_sorter.rb
@@ -1,9 +1,35 @@
+class String
+  def to_i_or_s
+    if to_i.to_s == self
+      to_i
+    else
+      self
+    end
+  end
+end
+
 # stolen from http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/53779
 module VersionSorter
   extend self
 
   def sort(list)
     list.sort { |a, b| versioncmp(a, b) }
+  end
+
+
+  def array_sort_hash(list)
+    hash = list.inject({}) do |acc, v|
+      acc[v.scan(/(\d+|[[:alpha:]]+)/).flatten.collect { |p| p.to_i_or_s }] = v
+      acc
+    end
+    hash.keys.sort.collect { |k| hash[k] }
+  end
+  
+  def array_sort_array(list)
+    list = list.inject([]) do |acc, v|
+      acc << [v.scan(/(\d+|[[:alpha:]]+)/).flatten.collect { |p| p.to_i_or_s }, v]
+    end
+    list.sort.collect { |k| k[-1] }
   end
 
   def rsort(list)
@@ -56,6 +82,20 @@ if $0 == __FILE__
       assert_equal sorted_versions, sort(versions)
     end
 
+    def test_array_sort_hash_sorts_verisons_correctly
+      versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
+      sorted_versions = %w( 1.0.9 1.0.9a 1.0.10 2.0 3.1.4.2 )
+
+      assert_equal sorted_versions, array_sort_hash(versions)
+    end
+
+    def test_array_sort_array_sorts_verisons_correctly
+      versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
+      sorted_versions = %w( 1.0.9 1.0.9a 1.0.10 2.0 3.1.4.2 )
+
+      assert_equal sorted_versions, array_sort_array(versions)
+    end
+
     def test_reverse_sorts_verisons_correctly
       versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
       sorted_versions = %w( 3.1.4.2 2.0 1.0.10 1.0.9a 1.0.9 )
@@ -65,10 +105,12 @@ if $0 == __FILE__
   end
 
   require 'benchmark'
-  versions = IO.read('tags.txt').split("\n")
+  versions = IO.read('tags_without_X.X.X.txt').split("\n")
   count = 10
   Benchmark.bm(20) do |x|
     x.report("sort")             { count.times { VersionSorter.sort(versions) } }
+    x.report("array_sort_hash")  { count.times { VersionSorter.array_sort_hash(versions) } }
+    x.report("array_sort_array") { count.times { VersionSorter.array_sort_array(versions) } }
   end
   puts
 
